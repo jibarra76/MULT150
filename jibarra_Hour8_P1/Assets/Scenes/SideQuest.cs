3155bf7a5a06ada2f4a0005c8a60e010ad50fc33
jibarra76 <jibarra76@gmail.com>
2026-03-29 14:50:16 -0700

    Hour 8 Pt 1
    More coding!

---
+++ jibarra_Hour8_P1/Assets/Scenes/SideQuest.cs
@@ -0,0 +1,23 @@
+/*
+__    __  ______        __    __  ________  __     __  ______  __    __ 
+|  \  |  \|      \      |  \  /  \|        \|  \   |  \|      \|  \  |  \
+| $$  | $$ \$$$$$$      | $$ /  $$| $$$$$$$$| $$   | $$ \$$$$$$| $$\ | $$
+| $$__| $$  | $$        | $$/  $$ | $$__    | $$   | $$  | $$  | $$$\| $$
+| $$    $$  | $$        | $$  $$  | $$  \    \$$\ /  $$  | $$  | $$$$\ $$
+| $$$$$$$$  | $$        | $$$$$\  | $$$$$     \$$\  $$   | $$  | $$\$$ $$
+| $$  | $$ _| $$_       | $$ \$$\ | $$_____    \$$ $$   _| $$_ | $$ \$$$$
+| $$  | $$|   $$ \      | $$  \$$\| $$     \    \$$$   |   $$ \| $$  \$$$
+ \$$   \$$ \$$$$$$       \$$   \$$ \$$$$$$$$     \$     \$$$$$$ \$$   \$$
+ 
+ TIY Questions:
+
+ 1. The method's name is TakeDamage    
+ 2. The variable type the method returns is an integer.
+ 3. There is one parameter and it is int damageAmount
+ 4. The code in the method's block is:
+	{
+		int health = 100;
+		return health - damageAmount;
+	}
+
+	*/
