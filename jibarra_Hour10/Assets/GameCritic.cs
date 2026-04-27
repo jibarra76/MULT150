6c23f753e0b2e576c78321865071710cd728f0b8
jibarra76 <jibarra76@gmail.com>
2026-04-26 17:31:14 -0700

    jibarra_Hour10
    I tried my best! :D

---
+++ jibarra_Hour10/Assets/GameCritic.cs
@@ -0,0 +1,49 @@
+﻿/*
+		
+ ██████╗  █████╗ ███╗   ███╗███████╗     ██████╗██████╗ ██╗████████╗██╗ ██████╗
+██╔════╝ ██╔══██╗████╗ ████║██╔════╝    ██╔════╝██╔══██╗██║╚══██╔══╝██║██╔════╝
+██║  ███╗███████║██╔████╔██║█████╗      ██║     ██████╔╝██║   ██║   ██║██║     
+██║   ██║██╔══██║██║╚██╔╝██║██╔══╝      ██║     ██╔══██╗██║   ██║   ██║██║     
+╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗    ╚██████╗██║  ██║██║   ██║   ██║╚██████╗
+ ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝     ╚═════╝╚═╝  ╚═╝╚═╝   ╚═╝   ╚═╝ ╚═════╝
+
+ Answers to "Improving the Game":
+
+ 1. Is the game too easy or hard?
+	A: I would say it's kind of in-between. It's really hard to keep track of the colored balls.
+	There isn't much to the game at all, making learning easy, but it's hard tracking the balls.
+
+ 2. What would make it easier or harder?
+	A: Perhaps varying the speed of the chaos balls could make it either easier or harder, depending.
+	Maybe if there were sevreal different goals for each ball? Varying the bounciness of the walls
+	and/or balls?
+
+ 3. What woud give it a "wow" factor?
+	A: Varying the physics/bounciness of variuos elements. Perhaps something on the ground that makes
+	the colored balls pick up speed when rolled over. Creating pitfalls you must avoid.
+	A way of extending the length of the bumper.
+ 
+ 4. What parts of the game are fun? What parts of the game are tedious?
+	A: Not much fun to be had really. It's tedious trying to track the balls. It could do with better
+	sound effects and a soundtrack.
+
+
+4 Suggestions from the book's Exercise:
+
+1. I added lava, because I am very unoriginal!
+
+2. I changed he Chaos Balls to rocks. I tried manipulating their RigidBodies, but I made it worse.
+
+3. I chaned the bumper into a slab of stone. I tried messing with the settings, but it's no longer working right. :
+
+4. I aded a few sound effects. Idon't know how to trigger the sounds when they hit an obstacle.
+
+Note: So it no longe works correctly after my additions. Couldn't figure out the problem. But I'm sure it's an easy fix?
+
+ */
+                                                                               
+
+
+	
+
+
