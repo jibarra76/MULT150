ec8fb03e6c4d8ac2f41203e2b2cf46ab668af3ad
jibarra76 <jibarra76@gmail.com>
2026-04-29 15:25:06 -0700

    Hour 11
    Part 1

---
+++ jibarra_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PrefabGenerator : MonoBehaviour
+{
+	public GameObject prefab;
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.B))
+		{
+			Instantiate(prefab);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Instantiate(prefab, transform.position, transform.rotation);
+		}
+	}
+}
