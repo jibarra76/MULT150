40406a8ae62fccfdb5de73bba61789f17d6037a6
jibarra76 <jibarra76@gmail.com>
2026-05-04 17:36:53 -0700

    Hour 11
    I re-did the assignment, and am now turning it in late!! :(

--- jibarra_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ jibarra_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -1,19 +1,33 @@
 using UnityEngine;
 
 public class PrefabGenerator : MonoBehaviour
+/* Hi professor Chao, I tried a few different things, and I still can't 
+figure it out :(
+I tried going to tutoring today (5/4), which is why the assignment is late. 
+But no one showed up to tutoring. I will turn it in as-is, and take my lumps!
+Thank you! */
 {
-	public GameObject prefab;
+	public GameObject Streetlight;
+	public float spacing = 3f;
 
-	void Update()
+	void Start()
 	{
+		for (int i = 0; i < 10; i++)
+			{
+				Vector3 spawnPosition = new Vector3(i * spacing, 0f, 0f);
+				Instantiate(Streetlight, spawnPosition, Quaternion.identity);
+			}
+	}
+	void Update()
+	{	
 		if (Input.GetKeyDown(KeyCode.B))
 		{
-			Instantiate(prefab);
+			Instantiate(Streetlight);
 		}
 
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
-			Instantiate(prefab, transform.position, transform.rotation);
+			Instantiate(Streetlight, transform.position, transform.rotation);
 		}
 	}
 }
