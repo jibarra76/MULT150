1f06892d9faf926c422ad7f0c1662797ebe06077
jibarra76 <jibarra76@gmail.com>
2026-05-07 12:00:52 -0700

    Hour 14
    Hour 14 complete. Now starting Hour 16, so this will change...

---
+++ jibarra_Hour14and16/Assets/Scripts/UserInterface.cs
@@ -0,0 +1,22 @@
+/* Hi class and professor!
+I thought I'd ake my dog Puki the star of the game.
+
+I found an image of Puki, deleted the background and added
+a gradient in Photoshop, and resized it to the exact
+specifications of the Canvas in Unity, so that the image
+of Puki would fit perfectly in the Unity Canvas
+
+I played around quite a bit with the fonts and font colors 
+for the text in Unity, choosing one I lied best, which captured
+the mood of the game and provided enough contrast.
+
+For the button, I randonly selected different colors for the state,
+but kept the same font as the rest of the text.
+
+I'm still trying to figure out the LeevlManager script so that I can 
+get the button to work properly. Hopefully I figure it out before 
+I submit the assignment.
+
+Thank you and enjoy!
+
+*/'
