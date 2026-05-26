deaf38b9eedb7ca106c73d42839e12ea2ebdf733
jibarra76 <jibarra76@gmail.com>
2026-05-26 14:55:53 -0700

    Hour 20
    I tried! :( Bout couldn't get everything done that was required...

---
+++ jibarra_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,55 @@
+/*	
+    ____  ___________ ____  ____  _   _______ ___________
+   / __ \/ ____/ ___// __ \/ __ \/ | / / ___// ____/ ___/
+  / /_/ / __/  \__ \/ /_/ / / / /  |/ /\__ \/ __/  \__ \ 
+ / _, _/ /___ ___/ / ____/ /_/ / /|  /___/ / /___ ___/ / 
+/_/ |_/_____//____/_/    \____/_/ |_//____/_____//____/  
+
+**SORRY I am turning this assignment in late. I had to go to Ensenada to deal with a serious family matter.
+I actually finisned Gauntlet Runner in time, but I haven't had a chance to make the requested mods.
+                                                         
+1. The game is easy to understand, but a little hard to manuever.
+	The controls are very stiff and the runner is slow.
+
+2. I would allow the runner to jump, and be allowed to vary speed (go slow and fast).
+	That would make it easier to avoid obstacles. I would also add other objects that
+	could hinder your speed or give you a speed boost.
+
+3. More variety. More obstacles. Speed variation. Power-ups. A more clear goal.
+	Maybe you can unlock different clothing to change the runner into as you pass certain distances.
+	And maybe each clothing item makes you a little better (shoes make you faster, jacket makes you bounce
+	off obstalces, which allows you to reoover to your regular speed sooner). 
+	Better music & SFX. A UI screen that shows your best time, and itmes you have collected and can equip.
+
+4. Not many parts to be honest! I like the character's mohawk. 
+
+5. She's too slow when running from side-to-side. Not being able to jump. No way to keep track of you best time.
+
+---------------------------------------------------------------	
+    __  _______  ____  _______________________  ______________  _   _______
+   /  |/  / __ \/ __ \/  _/ ____/  _/ ____/   |/_  __/  _/ __ \/ | / / ___/
+  / /|_/ / / / / / / // // /_   / // /   / /| | / /  / // / / /  |/ /\__ \ 
+ / /  / / /_/ / /_/ // // __/ _/ // /___/ ___ |/ / _/ // /_/ / /|  /___/ / 
+/_/  /_/\____/_____/___/_/   /___/\____/_/  |_/_/ /___/\____/_/ |_//____/  
+                                                                           
+1. Obstacle:
+	- I attempted to make an original shape (you can see it), but couldn't figure out how to make it the mesh for the Obstacle prefab.
+	- I had some Donut shapes and materials from my previous "Puki's Revenge" assignment. I got them from the 
+		Assets store. I replaced the Onstacle prefab with a donut shape, and made it slightly bigger.
+	- I attempted to make a 3rd Obstacle (noter donut), but I couldn't figure out how to add it.
+
+2: Powerup:
+	- I tried making my own model, but was unsuccessful, so I just replaced the shape w/ the"Phased Out" model, 
+		and I thought it made a cool effect.
+	- I changed the color to a "TOXIC GREEN for no real reason!	
+	- I went crazy with the Emission RoT set to 1000) just to see what happens. I like it!
+	- I raised the Start Size to 0.5. I like it!
+	- I raised the Start Speed.
+	- Color over Lifetime to RED
+	- I went crazy with Noise
+	- Unity began CRASHING! :D So I stopped here.
+
+
+
+
+*/
