60280e443189b0f9be70b44e837bb835d81e7242
jibarra76 <jibarra76@gmail.com>
2026-05-31 20:09:34 -0700

    Final!!
    Yay! Thanks for the great semester. I've learned so much, and will be immediately putting to work what I learned in my work! Have a great summer

---
+++ Modifications.cs
@@ -0,0 +1,61 @@
+﻿public class Modifications
+{/*
+    ░▒▓████████▓▒░▒▓█▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓██████▓▒░ ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░ */
+
+
+    // Choose 1 of the games below and list modifications 
+    // Delete the other 2 games
+
+    /*  ================================================================
+        CHAOS BALL
+        ================================================================
+        1 - directional light (colour, intensity, etc)
+        modifications: 
+
+        2 - fog
+        modifications: 
+
+        3 - create & apply skybox and customise it (atmosphere thickness, sky tint, exposure, etc)
+        modifications: 
+
+        CHAOS BALLS
+        4 - convert a chaos ball into a prefab, and replace all of them in the scene with instances of that prefab
+        [no record necessary]
+
+        5 - add particle system to the chaos ball prefab and customise at least 3 parameters
+        -All instances will update to include the particle system
+        coloured balls
+        parameter 1: 
+        parameter 2: 
+        parameter 3: 
+
+        6 - convert a coloured ball into a prefab, create variants for each of the colours, and replace them in the scene
+        -Don't forget to update the tags so that the game works
+        [no record necessary]
+
+        7 - apply at least 1 new texture while retaining the different colours
+        (add source url or package name)
+        texture source: 
+
+        GOALS
+        8 - modify light components (colour, range, intensity, etc)
+        modifications: 
+
+        WALL
+        9 - convert each to complex objects comprised of 3 or more shapes
+        -Try recreating them to be more interesting! castle walls, fences, pillars, etc
+        [no record necessary]
+
+        10 - use gameobject hierarchy to keep the wall prefab organised & apply at least 1 new texture
+        (add source url or package name)
+        texture source: 
+
+    */
+
+}
---
+++ jibarra_Final/Assets/Modifications.cs
@@ -0,0 +1,63 @@
+﻿public class Modifications
+{/*
+    ░▒▓████████▓▒░▒▓█▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓██████▓▒░ ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░ */
+
+
+    // Choose 1 of the games below and list modifications 
+    // Delete the other 2 games
+
+    /*  ================================================================
+        CHAOS BALL
+        ================================================================
+        1 - directional light (colour, intensity, etc)
+        modifications: Cool purple color! Raised the intensity. Added a cool Cookie.
+
+        2 - fog
+        modifications: Puke green color! Raised the strength and even added a Cookie for fun.
+
+        3 - create & apply skybox and customise it (atmosphere thickness, sky tint, exposure, etc)
+        modifications: Thiscniess to 3.31, lowered the exposure. Sky Tint is green, Ground is purple.
+
+        CHAOS BALLS
+        4 - convert a chaos ball into a prefab, and replace all of them in the scene with instances of that prefab
+        [no record necessary]
+
+        5 - add particle system to the chaos ball prefab and customise at least 3 parameters
+        -All instances will update to include the particle system
+        coloured balls
+        parameter 1:  Lowered Duration
+        parameter 2: Color over Lifetime (Gradient)
+        parameter 3: Shape (I like donuts)
+        * I made several more modifications to the Particle System!
+
+        6 - convert a coloured ball into a prefab, create variants for each of the colours, and replace them in the scene
+        -Don't forget to update the tags so that the game works
+        [no record necessary]
+
+        7 - apply at least 1 new texture while retaining the different colours
+        (add source url or package name)
+        texture source: I addeed a water texture from the Environments/Water (w/ more modifications)
+
+        GOALS
+        8 - modify light components (colour, range, intensity, etc)
+        modifications: I adjused several parameters of each Goal light, to my liking.
+
+        WALL
+        9 - convert each to complex objects comprised of 3 or more shapes
+        -Try recreating them to be more interesting! castle walls, fences, pillars, etc
+        [no record necessary]
+
+        10 - use gameobject hierarchy to keep the wall prefab organised & apply at least 1 new texture
+        (add source url or package name)
+        texture source: I used Palm Bark texture, and adjusted the parameteres to get it that metallic look.
+
+
+    */
+
+}
